879199720accb2ce194dde7f428a98b18435ff90
Josh Leitzel <joshleitzel@gmail.com>
2016-10-12 15:56:35 -0700

    Do nothing for now

--- opsworks_sidekiq_standalone/recipes/service.rb
+++ opsworks_sidekiq_standalone/recipes/service.rb
@@ -3,16 +3,16 @@ service "monit" do
   action :nothing
 end
 
-node[:deploy].each do |application, deploy|
-  execute "restart API" do
-    command "monit restart #{application}_api"
-  end
+# node[:deploy].each do |application, deploy|
+#   execute "restart API" do
+#     command "monit restart #{application}_api"
+#   end
 
-  execute "restart sidekiq worker 1" do
-    command "monit restart sidekiq_#{application}-worker1"
-  end
+#   execute "restart sidekiq worker 1" do
+#     command "monit restart sidekiq_#{application}-worker1"
+#   end
 
-  execute "restart sidekiq worker 2" do
-    command "monit restart sidekiq_#{application}-worker2"
-  end
-end
+#   execute "restart sidekiq worker 2" do
+#     command "monit restart sidekiq_#{application}-worker2"
+#   end
+# end
