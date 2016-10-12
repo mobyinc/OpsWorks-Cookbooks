d0c694fa25a55a9805ba975ccdc695d6d19c24ef
Josh Leitzel <joshleitzel@gmail.com>
2016-10-12 15:44:15 -0700

    Remove nothings

--- opsworks_sidekiq_standalone/recipes/service.rb
+++ opsworks_sidekiq_standalone/recipes/service.rb
@@ -6,16 +6,13 @@ end
 node[:deploy].each do |application, deploy|
   execute "restart API" do
     command "monit restart #{application}_api"
-    action :nothing
   end
 
   execute "restart sidekiq worker 1" do
     command "monit restart sidekiq_#{application}-worker1"
-    action :nothing
   end
 
   execute "restart sidekiq worker 2" do
     command "monit restart sidekiq_#{application}-worker2"
-    action :nothing
   end
 end
