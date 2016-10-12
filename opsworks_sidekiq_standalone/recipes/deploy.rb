01ae85ac5bf6585cfa7b4c69a82bea511bbff4bb
Josh Leitzel <joshleitzel@gmail.com>
2016-10-12 15:16:07 -0700

    Don't invoke main  recipe before custom one

--- opsworks_sidekiq_standalone/recipes/deploy.rb
+++ opsworks_sidekiq_standalone/recipes/deploy.rb
@@ -1,6 +1,5 @@
 # Adapted from deploy::rails: https://github.com/aws/opsworks-cookbooks/blob/master/deploy/recipes/rails.rb
 
-include_recipe 'deploy'
 include_recipe "opsworks_sidekiq_standalone::service"
 
 node[:deploy].each do |application, deploy|
