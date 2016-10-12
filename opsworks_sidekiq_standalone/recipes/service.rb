08c05726a26ee97f6a39270e98fe35ca9f086269
Josh Leitzel <joshleitzel@gmail.com>
2016-10-12 15:32:06 -0700

    Use correct syntax for multiple commands

--- opsworks_sidekiq_standalone/recipes/service.rb
+++ opsworks_sidekiq_standalone/recipes/service.rb
@@ -4,9 +4,18 @@ service "monit" do
 end
 
 node[:deploy].each do |application, deploy|
-  execute "restart app #{application}" do
-    command "monit restart sidekiq_#{application}-worker1"
-    command "monit restart sidekiq_#{application}-worker2"
+  execute "restart API" do
     command "monit restart #{application}_api"
+    action :nothing
+  end
+
+  execute "restart sidekiq worker 1" do
+    command "monit restart sidekiq_#{application}-worker1"
+    action :nothing
+  end
+
+  execute "restart sidekiq worker 2" do
+    command "monit restart sidekiq_#{application}-worker2"
+    action :nothing
   end
 end
