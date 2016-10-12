ba4e7e577a698cbaac9d376a3643d59cf6eb7372
Josh Leitzel <joshleitzel@gmail.com>
2016-10-12 15:10:45 -0700

    Remove the nothing action

--- opsworks_sidekiq_standalone/recipes/service.rb
+++ opsworks_sidekiq_standalone/recipes/service.rb
@@ -3,16 +3,10 @@ service "monit" do
   action :nothing
 end
 
-
 node[:deploy].each do |application, deploy|
-
-  # Overwrite the unicorn restart command declared elsewhere
-  # Apologies for the `sleep`, but monit errors with "Other action already in progress" on some boots.
   execute "restart app #{application}" do
     command "monit restart sidekiq_#{application}-worker1"
     command "monit restart sidekiq_#{application}-worker2"
     command "monit restart #{application}_api"
-    action :nothing
   end
-
 end
