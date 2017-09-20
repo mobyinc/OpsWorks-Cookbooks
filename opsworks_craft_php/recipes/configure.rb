374f6d7713af8765d435f4544aa4480533e06f64
James Jacoby <james@builtbymoby.com>
2017-09-20 13:47:31 -0700

    adds ad hoc commands required to make craft work well

--- opsworks_craft_php/recipes/configure.rb
+++ opsworks_craft_php/recipes/configure.rb
@@ -6,4 +6,18 @@ node[:deploy].each do |application, deploy|
     owner 'root'
     group 'root'
   end
+
+  # Ensure phg mycrypt command is run
+  execute "execute php5enmod mcrypt" do
+    command "sudo php5enmod mcrypt"
+    action :nothing
+  end
+
+  # Upgrade node to 6.11.1
+  execute "upgrade node version" do
+    command "sudo npm cache clean -f; sudo npm install -g n; sudo n 6.11.1"
+    action :nothing
+  end
 end
+
+
